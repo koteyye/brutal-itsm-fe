36c6d9a55987f09d9b2e0768e329fc90a0133cb4
apetukhov <koteyye@gmail.com>
2023-03-30 02:46:25 +0300

    add search component

--- src/components/UI/select/Select.jsx
+++ src/components/UI/select/Select.jsx
@@ -1,17 +1,29 @@
 import style from "./Select.module.css"
 import {useQuery} from "react-query";
-import Select from "react-select";
+import {useState} from "react";
 
 const SelectField = (props) => {
+
+    const [selectedValue, setSeletedValue] = useState()
 
     const {data, isLoading, error} =useQuery('roleList', props.values)
     if (isLoading) return <p>Загрузка...</p>
 
-    console.log(data.data)
+
+
+    const getValue = () => {
+        return selectedValue ? data.data.find(c => c.id === selectedValue) : ''
+    }
+
+    const onChange = (newValue) => {
+        setSeletedValue(newValue.id)
+    }
 
     return (
         <div className={style.select}>
-            <Select options={data.data} />
+            <select value={getValue()} onChange={onChange}>
+                {data.data ? data.data.map(v => <option key={v.id} value={v.id}>{v.name}</option>) : null}
+            </select>
         </div>
     )
 }
